3333a92425d413845713a0be75dbdf8475cd289f
unknown <tovar.ivan234@gmail.com>
2021-11-30 14:40:44 -0600

    Codigo completo

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-let arrayregistros= [];
+let RegistrosHTML = document.getElementById('RegistrosHTML');
+let arrayRegistros = [];
 
-//Cominucacion con el servidor 
-async function LeeRegistros(nombre){
+async function leeRegistros(){
 
-    const respuesta= await fetch("http://api-rest.mvlwver3.online/index.php?nombre=Ivan&apellido=Tovar");
-    const registros = await respuesta.json();
-
+    const respuesta = await fetch("data/registros.json");
+    const registros = await respuesta.json();    
+        
     return registros;
 }
 
 async function muestraRegistros(){
-   try{
-        arrayregistros =  await LeeRegistros();
-   }
-   catch (e){
-       console.log("Error generado: " + e);
-   }
-
-   console.log(arrayregistros);
+    try {
+        arrayRegistros = await leeRegistros();
+    } catch(e){
+        console.log("Error generado: " + e);
+    }
+    console.log(arrayRegistros); 
+    arrayRegistros.forEach(function(item, index){
+        RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + item.Nombre + " " + item.Apellido +  " " + item.Telefono + "<br>";
+    });
+    //RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + arrayRegistros.nombre + " " + arrayRegistros.apellido;  
 }
 
 muestraRegistros();
